90faa43fb11cccf48c63df7eec3c2233ed73d040
congtm <congtm.bhsoft@gmail.com>
2024-01-25 18:13:10 +0700

    fetch apt signup

--- frontend/src/fetch/api.js
+++ frontend/src/fetch/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 
-const api = axios.create({
-    baseURL: 'http://localhost:8000'
-})
+// const api = axios.create({
+//     baseURL: 'http://localhost:8000'
+// })
 
-export default api;
+// export default api;
